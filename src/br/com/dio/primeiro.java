4961fcfce6dc05abb8687475ff3ee1062e61a32b
AnaFerreira B <analuizaferreirabraganca1@gmail.com>
2022-07-19 16:43:25 -0300

    first commit

---
+++ src/br/com/dio/primeiro.java
@@ -0,0 +1,14 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class primeiro {
+    public static void main(String[] args) {
+        Gato gato = new Gato();
+        System.out.println(gato);
+
+       /* int a = 5;
+        int b = 7;
+        System.out.println("ola mundo! " + (a+b));*/
+    }
+}
